d441bed6fd153ce56c0e9cd72e763fda2b40901f
Rr42 <ramaragam@gmail.com>
2020-08-27 20:40:51 +0530

    Updated script to cleanup after update

--- kicad-updater.sh
+++ kicad-updater.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # ---------------------------------------------------------
 # Dev: Ramana R (github@Rr42)
-# Script version: v0.1
+# Script version: v0.2
+# Description:
+#   This is a WSL/Linux script to automatically update 
+#  KiCAD libraries from their repositories.
 # ---------------------------------------------------------
 # KiCAD libraty links
 # Symbols   : https://github.com/KiCad/kicad-symbols
@@ -10,74 +13,86 @@
 # Templates : https://github.com/kicad/kicad-templates
 # Unstall command: sudo xargs rm < $source/install_manifest.txt
 # Updating symbols
-printf "[KiCADupdater] Updating symbols"
-source=./kicad-symbols
-dest=./build
+printf "[KiCAD-updater] Updating symbols"
+source=./kicad-symbols/build
+dest=../output
 if [ ! -d $source ]
 then
     error=`git clone https://github.com/KiCad/kicad-symbols 2>&1`
+    error=`mkdir $source 2>&1`
 fi
 printf "."
 cd $source
-error=`cmake . 2>&1`
+error=`cmake .. 2>&1`
 printf "."
 error=`cmake -DCMAKE_INSTALL_PREFIX=$dest -P cmake_install.cmake 2>&1`
 printf "."
 error=`cp -r $dest/share "/mnt/c/Program Files/KiCad" 2>&1`
+printf "."
+error=`rm -r $dest 2>&1`
 printf "."
 cd ../
 printf "done\n"
 # Updating packages3d
-printf "[KiCADupdater] Updating packages3d"
-source=./kicad-packages3d
-dest=./build
+printf "[KiCAD-updater] Updating packages3d"
+source=./kicad-packages3d/build
+dest=../output
 if [ ! -d $source ]
 then
     error=`git clone https://github.com/kicad/kicad-packages3d 2>&1`
+    error=`mkdir $source 2>&1`
 fi
 printf "."
 cd $source
-error=`cmake . 2>&1`
+error=`cmake .. 2>&1`
 printf "."
 error=`cmake -DCMAKE_INSTALL_PREFIX=$dest -P cmake_install.cmake 2>&1`
 printf "."
 error=`cp -r $dest/share "/mnt/c/Program Files/KiCad" 2>&1`
+printf "."
+error=`rm -r $dest 2>&1`
 printf "."
 cd ../
 printf "done\n"
 # Updating footprints
-printf "[KiCADupdater] Updating footprints"
-source=./kicad-footprints
-dest=./build
+printf "[KiCAD-updater] Updating footprints"
+source=./kicad-footprints/build
+dest=../output
 if [ ! -d $source ]
 then
     error=`git clone https://github.com/KiCad/kicad-footprints 2>&1`
+    error=`mkdir $source 2>&1`
 fi
 printf "."
 cd $source
-error=`cmake . 2>&1`
+error=`cmake .. 2>&1`
 printf "."
 error=`cmake -DCMAKE_INSTALL_PREFIX=$dest -P cmake_install.cmake 2>&1`
 printf "."
 error=`cp -r $dest/share "/mnt/c/Program Files/KiCad" 2>&1`
 printf "."
+error=`rm -r $dest 2>&1`
+printf "."
 cd ../
 printf "done\n"
 # Updating templates
-printf "[KiCADupdater] Updating templates"
-source=./kicad-templates
-dest=./build
+printf "[KiCAD-updater] Updating templates"
+source=./kicad-templates/build
+dest=../output
 if [ ! -d $source ]
 then
     error=`git clone https://github.com/kicad/kicad-templates 2>&1`
+    error=`mkdir $source 2>&1`
 fi
 printf "."
 cd $source
-error=`cmake . 2>&1`
+error=`cmake .. 2>&1`
 printf "."
 error=`cmake -DCMAKE_INSTALL_PREFIX=$dest -P cmake_install.cmake 2>&1`
 printf "."
 error=`cp -r $dest/share "/mnt/c/Program Files/KiCad"  2>&1`
+printf "."
+error=`rm -r $dest 2>&1`
 printf "."
 cd ../
 printf "done\n"
